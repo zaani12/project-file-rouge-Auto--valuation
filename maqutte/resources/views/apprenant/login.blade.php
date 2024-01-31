be0cff45372950b5d2cf101a0ec31b0b747c162f
zaani12 <zaani.hamza.solicode@gmail.com>
2024-01-31 12:06:47 +0100

    push

--- maqutte/resources/views/apprenant/login.blade.php
+++ maqutte/resources/views/apprenant/login.blade.php
@@ -43,14 +43,14 @@
     </div>
     </form>
  
-    
+{{--     
     <p class="mb-1">
     <a href="forgot-password.html">I forgot my password</a>
     </p>
     <p class="mb-0">
     <a href="register.html" class="text-center">Register a new membership</a>
     </p>
-    </div>
+    </div> --}}
     
     </div>
     </div>
